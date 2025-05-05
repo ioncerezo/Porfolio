084edb8ccd7c96a643d3d33ab100641f21c5996a
Ion Cerezo <ioncerezovalero@gmail.com>
2025-05-05 14:47:52 +0200

    change folder name

--- actualizar_html.bat
+++ actualizar_html.bat
@@ -6,11 +6,11 @@ REM Transformar XML con XSL
 java -jar "%SAXON_JAR%" ^
  -s:"xml y xlst\porfolio.xml" ^
  -xsl:"xml y xlst\porfolio.xsl" ^
- -o:"html\index.html"
+ -o:"docs\index.docs"
 
 REM Copiar archivos CSS
-copy "xml y xlst\style.css" "html\style.css" >nul
-copy "xml y xlst\normalize.css" "html\normalize.css" >nul
+copy "xml y xlst\style.css" "docs\style.css" >nul
+copy "xml y xlst\normalize.css" "docs\normalize.css" >nul
 
 echo Todo listo. HTML y CSS copiados a la carpeta html.
 pause
